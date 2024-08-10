e53d9ae6ad830328afa0f4577202ff2fb31725f9
EchoZap <null>
2024-08-10 12:36:34 +0000

    🎉auto update by Gmeek action

--- docs/plugins/Ronantheme.js
+++ docs/plugins/Ronantheme.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {    
     let currentUrl = window.location.pathname;
 
-    if (currentUrl.includes('/index.html') || currentUrl.includes('/')) {
 
-        //主页主题------------------------------------------------------------------------------
+    //主页主题------------------------------------------------------------------------------
+    
+    if (currentUrl.includes('/index.html') || currentUrl.includes('/')) {
         console.log('应用主页主题');
         let style = document.createElement("style");
         style.innerHTML = `
@@ -44,108 +45,233 @@ document.addEventListener('DOMContentLoaded', function() {
         .pagination a:hover, .pagination a:focus, .pagination span:hover, .pagination span:focus, .pagination em:hover, .pagination em:focus {
             border-color: rebeccapurple;
         `;
-        document.head.appendChild(style);
+        document.head.appendChild(style);}
 
 
-    } else if (currentUrl.includes('/post/') || currentUrl.includes('/link.html') || currentUrl.includes('/about.html')) {
-
-        //文章页主题------------------------------------------------------------------------------
+    //文章页主题------------------------------------------------------------------------------
+    
+    else if (currentUrl.includes('/post/') || currentUrl.includes('/link.html') || currentUrl.includes('/about.html')) {
         console.log('文章页主题');
 
-    let style = document.createElement("style");
-    style.innerHTML = `
+        // 加载Clipboard.js库
+        let clipboardScript = document.createElement('script');
+        clipboardScript.type = 'text/javascript';
+        clipboardScript.src = 'https://cdn.jsdelivr.net/gh/EchoZap/echozap.github.io@main/static/plugins/clipboard.min.js';
+        document.body.appendChild(clipboardScript);
 
-    /* 主体布局 */
-    body {
-        min-width: 200px;
-        max-width: 1100px;
-        margin: 30px auto;
-        font-size: 16px;
-        font-family: sans-serif;
-        line-height: 1.25;
-        background: rgba(255, 255, 255, 0.85); /* 白色背景，透明度85% */
-        border-radius: 10px; /* 圆角边框 */
-        box-shadow: 0 0 10px rgba(0, 0, 0, 0.5); /* 添加阴影 */
-        overflow: auto;
-    }
+        let style = document.createElement("style");
+        style.innerHTML = `
 
-    @media (min-width: 1001px) {
-    body {
-        padding: 45px;
-    }
-    }
+        /* 主体布局 */
+        body {
+            min-width: 200px;
+            max-width: 1100px;
+            margin: 30px auto;
+            font-size: 16px;
+            font-family: sans-serif;
+            line-height: 1.25;
+            background: rgba(255, 255, 255, 0.85); /* 白色背景，透明度85% */
+            border-radius: 10px; /* 圆角边框 */
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.5); /* 添加阴影 */
+            overflow: auto;
+        }
 
-    @media (max-width: 1000px) {
-    body {
-        padding: 20px;
-    }
-    }
+        @media (min-width: 1001px) {
+        body {
+            padding: 45px;
+        }
+        }
+
+        @media (max-width: 1000px) {
+        body {
+            padding: 20px;
+        }
+        }
 
 
-    /* markdown内容 */
-    .markdown-body img {
-        border-radius: 10px;
-        border: 2px solid #a3e0e4;
-    }
+        /* markdown内容 */
+        .markdown-body img {
+            border-radius: 10px;
+            border: 2px solid #a3e0e4;
+        }
 
-    .markdown-alert {
-        border-radius: 10px;
-    }
+        .markdown-alert {
+            border-radius: 10px;
+        }
 
-    .markdown-body .highlight pre, .markdown-body pre {
-        color: #1d211f;
-        background-color: #f1f3f5;
-        box-shadow: 0 10px 30px 0 rgba(180, 174, 174, 0.4);
-        padding-top: 20px;
-        border-radius: 10px;
-    }
+        .markdown-body .highlight pre, .markdown-body pre {
+            color: #1d211f;
+            background-color: #f1f3f5;
+            box-shadow: 0 10px 30px 0 rgba(180, 174, 174, 0.4);
+            padding-top: 20px;
+            border-radius: 10px;
+        }
 
-    .markdown-body code, .markdown-body tt {
-        background-color: rgb(141 150 161 / 20%);
-    }
+        .markdown-body code, .markdown-body tt {
+            background-color: rgb(141 150 161 / 20%);
+        }
 
-    /*行内代码*/
-    .notranslate {
-        font-size: 16px;   /*默认.9rem*/
-        word-wrap: break-word;
-        padding: 2px 4px;
-        border-radius: 4px;
-        margin: 0 2px;
-        color:  rgb(239, 112, 96);;
-        background-color: rgba(27,31,35,.05);
-        font-family: Operator Mono, Consolas, Monaco, Menlo, monospace;
-        word-break: break-all;
-    }
+        /*行内代码*/
+        .notranslate {
+            font-size: 16px;   /*默认.9rem*/
+            word-wrap: break-word;
+            padding: 2px 4px;
+            border-radius: 4px;
+            margin: 0 2px;
+            color:  rgb(239, 112, 96);;
+            background-color: rgba(27,31,35,.05);
+            font-family: Operator Mono, Consolas, Monaco, Menlo, monospace;
+            word-break: break-all;
+        }
 
-    /* 标题 */
-    .markdown-body h1 {
-        font-size: 1.3rem;
-        border-bottom: 3px solid rgb(239, 112, 96);
-    }
+        /* 标题 */
+        .markdown-body h1 {
+            font-size: 1.3rem;
+            border-bottom: 3px solid rgb(239, 112, 96);
+        }
 
-    /* 大标题之间隔开一个空行距离 */
-    .markdown-body h1::before {
-        content: "\A";
-        white-space: pre; 
-    }
+        /* 大标题之间隔开一个空行距离 */
+        .markdown-body h1::before {
+            content: "\A";
+            white-space: pre; 
+        }
 
-    /* 小标题之间隔开一个空行距离 */
-    /* .markdown-body h2::before,
-    .markdown-body h3::before,
-    .markdown-body h4::before,
-    .markdown-body h5::before,
-    .markdown-body h6::before {
-        content: "\A";        
-        white-space: pre;       
-        display: block;         
-    } */
-    `;
+        /* 小标题之间隔开一个空行距离 */
+        /* .markdown-body h2::before,
+        .markdown-body h3::before,
+        .markdown-body h4::before,
+        .markdown-body h5::before,
+        .markdown-body h6::before {
+            content: "\A";        
+            white-space: pre;       
+            display: block;         
+        } */
+
+
+        /* 复制按钮----------------------------------------- */
+        .markdown-body .highlight pre, .markdown-body pre {
+            position: relative;
+        }
+
+        .copy-button {
+            position: absolute;
+            top: 10px;
+            right: 10px;
+            background-color: #ffffff; 
+            border: 1px solid #ddd; 
+            cursor: pointer;
+            border-radius: 5px; 
+            display: flex;
+            align-items: center;
+        }
+
+        pre.notranslate .copy-button:hover {
+            background-color: #f5f5f5; 
+        }
+
+        pre.notranslate .copied {
+            border: 1px solid #008000; 
+            position: absolute;
+            top: 10px;
+            right: 10px;
+            cursor: pointer;
+            font-size: 12px;
+            border-radius: 5px; 
+            display: flex;
+            align-items: center;
+        }
+
+        pre.notranslate .copied svg {
+            color: #008000; 
+        }
+
+        `;
         document.head.appendChild(style);
-
+        
+        // 复制按钮
+        clipboardScript.onload = function() {
+            // 获取所有代码块
+            var codeBlocks = document.querySelectorAll('pre.notranslate');
     
-    } else if (currentUrl.includes('/tag')) {
+            // 遍历
+            codeBlocks.forEach((codeBlock) => {
+                // 创建复制按钮
+                var copyButton = createCopyButton();
+    
+                codeBlock.appendChild(copyButton);
+    
+                // 初始化Clipboard.js
+                var clipboard = new ClipboardJS(copyButton, {
+                    target: function(trigger) {
+                        return codeBlock;
+                    }
+                });
+    
+                // 监听成功事件
+                clipboard.on('success', function(e) {
+                    handleCopySuccess(copyButton);
+                    e.clearSelection();
+                });
+    
+                // 监听代码块滚动事件
+                codeBlock.addEventListener('scroll', function() {
+                    adjustButtonPosition(copyButton, codeBlock);
+                    hideButton(copyButton)
+                });
+            });
+        };
+    
+        // 创建复制按钮
+        function createCopyButton() {
+            var button = document.createElement('button');
+            button.innerHTML = `
+                <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-copy js-clipboard-copy-icon m-2">
+                    <path d="M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 0 1 0 1.5h-1.5a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-1.5a.75.75 0 0 1 1.5 0v1.5A1.75 1.75 0 0 1 9.25 16h-7.5A1.75 1.75 0 0 1 0 14.25Z"></path><path d="M5 1.75C5 .784 5.784 0 6.75 0h7.5C15.216 0 16 .784 16 1.75v7.5A1.75 1.75 0 0 1 14.25 11h-7.5A1.75 1.75 0 0 1 5 9.25Zm1.75-.25a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-7.5a.25.25 0 0 0-.25-.25Z"></path>
+                </svg>
+            `;
+            button.classList.add('copy-button');
+            return button;
+        }
+    
+        // 复制成功后操作
+        function handleCopySuccess(button) {
+            button.innerHTML = `
+                <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-check js-clipboard-copy-icon m-2">
+                    <path fill-rule="evenodd" d="M13.78 4.22a.75.75 0 010 1.06l-7.25 7.25a.75.75 0 01-1.06 0L2.22 9.28a.75.75 0 011.06-1.06L6 10.94l6.72-6.72a.75.75 0 011.06 0z"></path>
+                </svg>
+            `;
+            button.classList.add('copied');
+            button.classList.remove('copy-button');
+            setTimeout(function() {
+                button.innerHTML = `
+                    <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-copy js-clipboard-copy-icon m-2">
+                        <path d="M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 0 1 0 1.5h-1.5a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-1.5a.75.75 0 0 1 1.5 0v1.5A1.75 1.75 0 0 1 9.25 16h-7.5A1.75 1.75 0 0 1 0 14.25Z"></path><path d="M5 1.75C5 .784 5.784 0 6.75 0h7.5C15.216 0 16 .784 16 1.75v7.5A1.75 1.75 0 0 1 14.25 11h-7.5A1.75 1.75 0 0 1 5 9.25Zm1.75-.25a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-7.5a.25.25 0 0 0-.25-.25Z"></path>
+                    </svg>
+                `;
+                button.classList.remove('copied');
+                button.classList.add('copy-button');
+            }, 1000); // 1秒
+        }
+    
+        // 滚动时调整按钮位置
+        function adjustButtonPosition(button, codeBlock) {
+            button.style.right = (10 - codeBlock.scrollLeft) + 'px';
+        }
+    
+        // 滚动时隐藏按钮
+        function hideButton(button) {
+            clearTimeout(button.timeout);
+            button.style.display = 'none';
+            button.timeout = setTimeout(function() {
+                button.style.display = 'flex';
+            }, 1000);
+        }
+    } 
 
-        // 搜索页主题--------------------------------------------------------------------
+
+    // 搜索页主题--------------------------------------------------------------------
+    
+    else if (currentUrl.includes('/tag')) {
         console.log('应用搜索页主题');
         let style = document.createElement("style");
         style.innerHTML = `
@@ -254,4 +380,5 @@ document.addEventListener('DOMContentLoaded', function() {
     } else {
         console.log('未应用主题');
     }
+
 })
